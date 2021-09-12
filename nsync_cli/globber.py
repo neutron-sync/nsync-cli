3ed374a315cec246cf3f0bd0b98ca926840ff50e
Paul Bailey <paul@neutron.studio>
2021-09-11 22:06:01 -0500

    remove dead code

--- nsync_cli/globber.py
+++ nsync_cli/globber.py
@@ -14,28 +14,3 @@ def get_paths(path_glob):
 		ret.append(p)
 
 	return ret
-
-	if '*' in path_glob:
-		path = Path(path_glob)
-		if path.is_absolute():
-			parts = path_glob.split(os.sep)
-			abs_parts = []
-			glob_parts = []
-
-			for p in parts:
-				if glob_parts or '*' in p:
-					glob_parts.append(p)
-
-				else:
-					abs_parts.append(p)
-
-			print(os.sep.join(abs_parts))
-			path = Path(os.sep.join(abs_parts))
-			print(os.sep.join(glob_parts))
-			return path.glob(os.sep.join(glob_parts))
-
-		else:
-			return Path().glob(path_glob)
-
-	else:
-		return [Path(path_glob)]
